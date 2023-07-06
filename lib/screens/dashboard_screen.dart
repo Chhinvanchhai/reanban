264236d22a39e3f902e090bff8cdf9337f823737
Vanchhai <Vanchhai>
2023-07-06 21:20:23 +0700

    update responsive

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:reanban/constants.dart';
-import './header.dart';
+import 'package:responsive_ui/responsive_ui.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -9,11 +8,57 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Dashboard"),
-      ),
-      body: Container(
-        child: Column(children: [Text("homee")]),
+      body: Padding(
+        padding: EdgeInsets.all(defaultPadding),
+        child: SingleChildScrollView(
+          child: Responsive(runSpacing: 10, children: [
+            Div(
+              divison: Division(colL: 4, colM: 6, colS: 12, colXS: 12),
+              child: Container(
+                margin: EdgeInsets.all(defaultPadding / 2),
+                width: double.infinity,
+                height: 300,
+                decoration: BoxDecoration(color: Colors.red),
+              ),
+            ),
+            Div(
+              divison: Division(colL: 4, colM: 6, colS: 12, colXS: 12),
+              child: Container(
+                margin: EdgeInsets.all(defaultPadding / 2),
+                width: double.infinity,
+                height: 300,
+                decoration: BoxDecoration(color: Colors.red),
+              ),
+            ),
+            Div(
+              divison: Division(colL: 4, colM: 6, colS: 12, colXS: 12),
+              child: Container(
+                margin: EdgeInsets.all(defaultPadding / 2),
+                width: double.infinity,
+                height: 300,
+                decoration: BoxDecoration(color: Colors.red),
+              ),
+            ),
+            Div(
+              divison: Division(colL: 4, colM: 6, colS: 12, colXS: 12),
+              child: Container(
+                margin: EdgeInsets.all(defaultPadding / 2),
+                width: double.infinity,
+                height: 300,
+                decoration: BoxDecoration(color: Colors.red),
+              ),
+            ),
+            Div(
+              divison: Division(colL: 4, colM: 6, colS: 12, colXS: 12),
+              child: Container(
+                margin: EdgeInsets.all(defaultPadding / 2),
+                width: double.infinity,
+                height: 300,
+                decoration: BoxDecoration(color: Colors.red),
+              ),
+            )
+          ]),
+        ),
       ),
     );
   }
